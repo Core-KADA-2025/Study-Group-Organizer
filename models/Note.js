8bd332cb32b58f7d26b23e293fddf605e0e5a597
zafyrais <zafyrais@LAPTOP-RG9OS3GS.>
2025-07-09 17:56:34 +0700

    new note scheme

--- models/Note.js
+++ models/Note.js
@@ -4,6 +4,7 @@ const noteSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: String,
   room: { type: mongoose.Schema.Types.ObjectId, ref: 'Room', required: true },
+  group: { type: mongoose.Schema.Types.ObjectId, ref: 'Group', required: true },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Note', noteSchema);
