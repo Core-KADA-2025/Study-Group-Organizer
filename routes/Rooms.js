38450a2d27750374d4b7608f1136073987895549
satwika putri <satwika.zahrani@gmail.com>
2025-07-09 19:16:25 +0700

    Rooms models & routes

--- routes/Rooms.js
+++ routes/Rooms.js
@@ -3,30 +3,45 @@ const router = express.Router();
 const Room = require('../models/Room');
 const paginate = require('../middlewares/paginate');
 
+// Create a new room
 router.post('/', async (req, res) => {
-  const room = new Room(req.body);
-  await room.save();
-  res.status(201).json(room);
+  try {
+    const { name, group } = req.body;
+    const room = await Room.create({ name, group });
+    res.status(201).json(room);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
-router.get('/', paginate(Room, 'group'), (req, res) => {
-  res.json(res.paginatedResults);
+// Get all rooms
+router.get('/', async (req, res) => {
+  try {
+    const rooms = await Room.find().populate('group');
+    res.json(rooms);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
-router.get('/:id', async (req, res) => {
-  const room = await Room.findById(req.params.id).populate('group');
-  if (!room) return res.status(404).send('Not found');
-  res.json(room);
-});
-
+// Update a room
 router.put('/:id', async (req, res) => {
-  const room = await Room.findByIdAndUpdate(req.params.id, req.body, { new: true });
-  res.json(room);
+  try {
+    const updated = await Room.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updated);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
+// Delete a room
 router.delete('/:id', async (req, res) => {
-  await Room.findByIdAndDelete(req.params.id);
-  res.sendStatus(204);
+  try {
+    await Room.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Room deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 module.exports = router;
